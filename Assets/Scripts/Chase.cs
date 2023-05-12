d0d46337110b91ee4ac3ff76f62cb527764618cb
IkerPerezBralo <iker.perez.bralo@opendeusto.es>
2023-05-12 09:37:50 +0200

    Creado el movimiento con el click

--- Assets/Scripts/Chase.cs
+++ Assets/Scripts/Chase.cs
@@ -10,17 +10,27 @@ using UnityEngine.AI;
         public Transform[] goals;
         private NavMeshAgent agent;
 
+        private int i = 0;
+        private int goalCounter;
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
+            goalCounter = goals.Length;
 
         }
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance < 0.5)
         {
-            for (int i = 0; i < goals.Length; i++)
-            {
             agent.SetDestination(goals[i].position);
+            i++;
+            if(i == goalCounter)
+            {
+                i = 0;
             }
         }
+
+    }
     }
